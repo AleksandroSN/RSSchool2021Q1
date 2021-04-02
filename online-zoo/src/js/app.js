e7c73547d831263ae204736635e3739d709b9a93
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-04-02 15:16:04 +0300

    feat: added first screen, aside panel, footer and fix some styles

--- online-zoo/src/js/app.js
+++ online-zoo/src/js/app.js
@@ -7,5 +7,9 @@ import '../assets/ico/profile.svg';
 import '../assets/ico/burger_menu.svg';
 import '../assets/ico/rs_school_js.svg';
 import '../assets/ico/yem_digital.svg';
+import '../assets/ico/arrow.svg';
+import '../assets/ico/fb.svg';
+import '../assets/ico/insta.svg';
+import '../assets/ico/twitter.svg';
 
 console.log('Work');
