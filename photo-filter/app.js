fda5292916f271538eee41a9b841be4789a01813
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-04-25 15:49:15 +0300

    init: started task photo-filter

---
+++ photo-filter/app.js
@@ -0,0 +1,15 @@
+const fullScreenBtn = document.querySelector('.fullscreen');
+
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+};
+
+fullScreenBtn.addEventListener('click', toggleFullScreen);
+
+
