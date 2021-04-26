a0c046f035870b3180f7dfb4c2424116bed95cd3
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-04-26 20:18:36 +0300

    feat: finish task photo-filter

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -1,8 +1,5 @@
 const fullScreenBtn = document.querySelector('.fullscreen');
-const main = document.querySelector('main');
-const fragment = document.createDocumentFragment();
 const inputs = document.querySelectorAll('input[type=range]');
-const outputs = document.querySelectorAll('output');
 
 const toggleFullScreen = () => {
   if (!document.fullscreenElement) {
@@ -17,9 +14,6 @@ const toggleFullScreen = () => {
 fullScreenBtn.addEventListener('click', toggleFullScreen);
 
 function inputValue() {
-  console.log(this.value);
-  console.log(this.nextElementSibling);
-  console.log(this.dataset);
   this.nextElementSibling.value = this.value;
   document.documentElement.style.setProperty(
     `--${this.name}`,
@@ -33,13 +27,8 @@ const btnReset = document.querySelector('.btn-reset');
 
 function reset() {
   inputs.forEach((input) => {
-    if (input.name === 'saturate') {
-      input.value = 100;
-      input.nextElementSibling.value = input.value;
-    } else {
-      input.value = 0;
-      input.nextElementSibling.value = input.value;
-    }
+    input.value = input.defaultValue;
+    input.nextElementSibling.value = input.value;
   });
   document.documentElement.style = '';
 }
@@ -48,31 +37,70 @@ btnReset.addEventListener('click', reset);
 
 const buttonLoadPicture = document.querySelector('input[type=file]');
 const editor = document.querySelector('.editor');
-const imageContainer = editor.children[1];
+const imageContainer = editor.lastElementChild;
 
 function loadImg() {
   const file = buttonLoadPicture.files[0];
   const reader = new FileReader();
-  // console.log(buttonLoadPicture.files);
   reader.onload = () => {
-    const img = new Image();
-    img.src = reader.result;
-    editor.replaceChild(img, imageContainer);
-    editor.append(img);
+    imageContainer.src = reader.result;
   };
   reader.readAsDataURL(file);
+  buttonLoadPicture.value = null;
 }
 
 buttonLoadPicture.addEventListener('change', loadImg);
 
-// function renderCanvas() {
-//   const canvas = document.createElement('canvas');
-//   canvas.id = 'img';
-//   const ctx = canvas.getContext('2d');
-//   ctx.fillStyle = 'green';
-//   ctx.fillRect(10, 10, 100, 100);
-//   fragment.appendChild(canvas);
-//   main.appendChild(fragment);
-// }
+const nextImg = document.querySelector('.btn-next');
+const baseURL =
+  'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+let startNumber = 0;
 
-// renderCanvas();
+function newUrl() {
+  imageContainer.src = '';
+  const date = new Date();
+  const hours = date.getHours();
+
+  let urlResult = '';
+  let urlOnTime = '';
+  if (hours >= 6 && hours <= 12) {
+    urlOnTime = baseURL + 'morning/';
+  } else if (hours >= 12 && hours <= 18) {
+    urlOnTime = baseURL + 'day/';
+  } else if (hours >= 18 && hours <= 24) {
+    urlOnTime = baseURL + 'evening/';
+  } else urlOnTime = baseURL + 'nigth/';
+
+  startNumber = ((startNumber + 20) % 20) + 1;
+  startNumber >= 10
+    ? (urlResult = urlOnTime + `${startNumber}.jpg`)
+    : (urlResult = urlOnTime + `0${startNumber}.jpg`);
+
+  imageContainer.src = urlResult;
+}
+
+nextImg.addEventListener('click', newUrl);
+
+const savePictureBtn = document.querySelector('.btn-save');
+
+function downloadImg() {
+  const img = new Image();
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.src = imageContainer.src;
+  img.onload = function () {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext('2d');
+    ctx.filter = getComputedStyle(imageContainer).filter;
+    ctx.drawImage(img, 0, 0);
+
+    const link = document.createElement('a');
+    link.download = 'download.png';
+    link.href = canvas.toDataURL();
+    link.click();
+    link.delete;
+  };
+}
+
+savePictureBtn.addEventListener('click', downloadImg);
