54bd286e0704ae0a0b237f354d7a0d3e91289b92
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-06-18 16:25:29 +0300

    fix: remove bagged pagination

--- async-race/src/js/pages/garage.ts
+++ async-race/src/js/pages/garage.ts
@@ -1,27 +1,23 @@
-import { createCar, updateCar } from "../api/car-api";
-import { BaseComponent } from "../components/base-components";
-import { carsOnPage } from "../components/car/carTrack";
-import { GenerateCarForm } from "../sections/generateCar";
-import { paginationButtons } from "../sections/pagination";
-import { Race } from "../sections/raceSection";
-import { updateAndCreateWinner } from "../shared/updateAndCreateWinner";
-import { announcWinner } from "../templates/template";
-import { race } from "../utils/race";
-import {
-  countAllCars,
-  createRandomCars,
-  handlerPaginationBtn,
-} from "../utils/utils";
+import { createCar, updateCar } from '../api/car-api';
+import { BaseComponent } from '../components/base-components';
+import { carsOnPage } from '../components/car/carTrack';
+import { GenerateCarForm } from '../sections/generateCar';
+import { paginationButtons } from '../sections/pagination';
+import { Race } from '../sections/raceSection';
+import { updateAndCreateWinner } from '../shared/updateAndCreateWinner';
+import { announcWinner } from '../templates/template';
+import { race } from '../utils/race';
+import { countAllCars, createRandomCars, handlerPaginationBtn } from '../utils/utils';
 
 export class Garage extends BaseComponent {
   private readonly generateSection: GenerateCarForm;
 
   private readonly raceSection: Race;
 
-  private garagePage = localStorage.getItem("garagePage");
+  private garagePage = localStorage.getItem('garagePage');
 
   constructor() {
-    super("main", ["main"]);
+    super('main', ['main']);
     this.generateSection = GenerateCarForm.getInstance();
     this.raceSection = Race.getInstance();
     this.element.append(this.generateSection.element, this.raceSection.element);
@@ -33,82 +29,70 @@ export class Garage extends BaseComponent {
   }
 
   listenerForCreateCar() {
-    this.generateSection.createCarForm.element.addEventListener(
-      "submit",
-      async (evt: Event) => {
-        evt.preventDefault();
-        const inputsValue = this.generateSection.createCarForm.getInputsValue();
-        this.generateSection.createCarForm.element.reset();
-        await createCar(inputsValue);
-        this.raceSection.removeCars();
-        const totalCars = await this.raceSection.addCars();
+    this.generateSection.createCarForm.element.addEventListener('submit', async (evt: Event) => {
+      evt.preventDefault();
+      const inputsValue = this.generateSection.createCarForm.getInputsValue();
+      this.generateSection.createCarForm.element.reset();
+      await createCar(inputsValue);
+      this.raceSection.removeCars();
+      const totalCars = await this.raceSection.addCars();
 
-        handlerPaginationBtn(
-          Number(this.garagePage),
-          totalCars,
-          paginationButtons[1],
-          paginationButtons[0],
-          7
-        );
-      }
-    );
+      // handlerPaginationBtn(
+      //   Number(this.garagePage),
+      //   totalCars,
+      //   paginationButtons[1],
+      //   paginationButtons[0],
+      //   7
+      // );
+    });
   }
 
   listenerForUpdateCar() {
-    this.generateSection.updateCarForm.element.addEventListener(
-      "submit",
-      async (evt: Event) => {
-        evt.preventDefault();
-        const inputsValue = this.generateSection.updateCarForm.getInputsValue();
-        const idCar = Number(localStorage.getItem("carID"));
-        localStorage.removeItem("carID");
-        this.generateSection.updateCarForm.element.reset();
-        this.generateSection.updateCarForm.textInput.element.disabled = true;
-        await updateCar(idCar, inputsValue);
-        this.raceSection.removeCars();
-        this.raceSection.addCars();
-      }
-    );
+    this.generateSection.updateCarForm.element.addEventListener('submit', async (evt: Event) => {
+      evt.preventDefault();
+      const inputsValue = this.generateSection.updateCarForm.getInputsValue();
+      const idCar = Number(localStorage.getItem('carID'));
+      localStorage.removeItem('carID');
+      this.generateSection.updateCarForm.element.reset();
+      this.generateSection.updateCarForm.textInput.element.disabled = true;
+      await updateCar(idCar, inputsValue);
+      this.raceSection.removeCars();
+      this.raceSection.addCars();
+    });
   }
 
   listenGenerateCarsBtn() {
-    this.generateSection.generateCarBtn.element.addEventListener(
-      "click",
-      async () => {
-        await createRandomCars();
-        await countAllCars().then((response) => {
-          this.raceSection.countCars.textContent = response.count;
-        });
-        this.raceSection.removeCars();
-        const totalCars = await this.raceSection.addCars();
-        handlerPaginationBtn(
-          Number(this.garagePage),
-          totalCars,
-          paginationButtons[1],
-          paginationButtons[0],
-          7
-        );
-      }
-    );
+    this.generateSection.generateCarBtn.element.addEventListener('click', async () => {
+      await createRandomCars();
+      await countAllCars().then((response) => {
+        this.raceSection.countCars.textContent = response.count;
+      });
+      this.raceSection.removeCars();
+      const totalCars = await this.raceSection.addCars();
+      // handlerPaginationBtn(
+      //   Number(this.garagePage),
+      //   totalCars,
+      //   paginationButtons[1],
+      //   paginationButtons[0],
+      //   7
+      // );
+    });
   }
 
   listenRaceBtn() {
-    this.generateSection.raceBtn.element.addEventListener("click", async () => {
+    this.generateSection.raceBtn.element.addEventListener('click', async () => {
       this.generateSection.resetBtn.element.disabled = false;
       this.generateSection.raceBtn.element.disabled = true;
       const winner = race();
       winner.then(async (x) => {
         await updateAndCreateWinner(x.id, x);
-        this.raceSection.element.insertAdjacentHTML(
-          "beforeend",
-          announcWinner(x.carName, x.time)
-        );
+        this.raceSection.element.insertAdjacentHTML('beforeend', announcWinner(x.carName, x.time));
       });
     });
   }
 
   listenResetBtn() {
-    this.generateSection.resetBtn.element.addEventListener("click", () => {
+    this.generateSection.resetBtn.element.addEventListener('click', () => {
       this.raceSection.removeAnnounceWinner();
       this.generateSection.resetBtn.element.disabled = true;
       this.generateSection.raceBtn.element.disabled = false;
