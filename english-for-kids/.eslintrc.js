78a64ea16a8670ef2b603ea2a24b2123343d6888
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-06-25 23:05:03 +0300

    init: started english-for-kids task

---
+++ english-for-kids/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': 'warn',
+    'jsx-quotes': [1, 'prefer-double'],
+  },
+};
